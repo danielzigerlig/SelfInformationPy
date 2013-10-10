6466ee66892aadb0f82a250ad3e6354054b6c629
dzigerli <dzigerli@hsr.ch>
2013-10-10 18:10:14 +0200

    Initial version

---
+++ server.py
@@ -0,0 +1,23 @@
+import psutil
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+def get_memory_info():
+    memory = psutil.virtual_memory()
+    total = round(float(memory.total) / 1024 / 1024, 2)
+    available = round(float(memory.available) / 1024 / 1024, 2)
+    stats = []
+    stats.append('Total memory: %s MB' % total)
+    stats.append('Available memory: %s MB' % available)
+    stats.append('Percent used: %s%%' % memory.percent)
+    return stats
+
+@app.route("/")
+def stats():
+    stats = get_memory_info()
+    return render_template('stats.html', stats=stats)
+
+if __name__ == "__main__":
+    app.run()
